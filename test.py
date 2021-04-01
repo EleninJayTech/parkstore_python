2351151d037994d1d7e4a7282e328b168a41f820
eleninjaytech@gmail.com <@@akdlsqndn22>
2021-04-02 00:35:28 +0900

    초이템 가져올 상품 링크 수집

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import os
 import sys
 import time
 
+# 디바이스
 current_device = 'pc'
 if os.name == 'posix':
     # 리눅스
@@ -30,6 +31,7 @@ if current_device == 'linux':
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
 else:
+    # 유저 정보 추가
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36")
 
 # 브라우저 설정
@@ -42,8 +44,10 @@ browser = webdriver.Chrome(
 url = "http://choitemb2b.com/member/login.html"
 browser.get(url)
 
+# 지연 시간
 delay_term = 1
 
+# 로그인 정보 가져오기
 config = configparser.ConfigParser()
 config.read('./secure.ini')
 login_id = config['choitem']['id']
@@ -58,14 +62,42 @@ if input_id is not None:
     btn_login_submit = browser.find_element_by_css_selector("form[id^='member_form'] [onclick^='MemberAction.login']")
     btn_login_submit.click()
 
-# time.sleep(delay_term)
+# 원하는 카테고리
+category_list = [28]
+# 1 ~ 6 페이지
+page_list = range(1, 10)
+for cate_no in category_list:
+    # 브라우저 정보 기록
+    browser_info = []
+    # 페이지 이동 후 새탭 열기
+    for _page in page_list:
+        time.sleep(delay_term)
+        link_cate = 'http://choitemb2b.com/product/list.html?cate_no={}&sort_method=5&page={}'.format(cate_no, _page)
+        # 탭 이름
+        tab_name = 'cate_no_{}_{}'.format(cate_no, _page)
+        # 정보 추가
+        browser_info.append(tab_name)
+        # 새탭 열기
+        script_str = "window.open(\"{}\", \"{}\", 'location=yes');".format(link_cate, tab_name)
+        browser.execute_script(script_str)
+        # 브라우저 정보 순서대로 윈도우 번호 호출
+        move_tab_idx = browser_info.index(tab_name) + 1
+        # 탭 변경
+        browser.switch_to.window(browser.window_handles[move_tab_idx])
+        # 상품 있는지 확인
+        goods_items = browser.find_elements_by_css_selector('li.item.DB_rate')
+        # 상품 없는 페이지 일경우
+        if len(goods_items) == 0:
+            # 브라우저 정보 삭제
+            browser_info.remove(tab_name)
+            browser.close()
+            break
 
-링크_생활용품_신상품 = 'http://choitemb2b.com/product/list.html?cate_no=28&sort_method=5'
-창이름 = 'cate_no_28'
-
-script_str = "window.open(\"{}\", \"{}\", 'location=yes');".format(링크_생활용품_신상품, 창이름)
-browser.execute_script(script_str)
-browser.switch_to.window(browser.window_handles[1])
+    # 페이지별 상품 링크 수집
+    for tab_name in browser_info:
+        time.sleep(delay_term)
+        move_tab_idx = browser_info.index(tab_name) + 1
+        browser.switch_to.window(browser.window_handles[move_tab_idx])
 
 # browser.close()
 sys.exit()
